5d6720dfde59a9be1e09d0b78a395a834e668244
mudrakpatel <mudrakpatel96@gmail.com>
2017-05-19 10:39:01 +0530

    Event listener practice; Not from tutorials

--- app/src/main/java/apps/mudrak/androidapp1/MainActivity.java
+++ app/src/main/java/apps/mudrak/androidapp1/MainActivity.java
@@ -2,6 +2,10 @@ package apps.mudrak.androidapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //clickMeButton click event listener code
+        Button clickMeButton = (Button) findViewById(R.id.clickMeButton);
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You clicked the button!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        TextView helloWorldTextView = (TextView) findViewById(R.id.helloWorldtextView);
+        helloWorldTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"You clicked the TextView!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
